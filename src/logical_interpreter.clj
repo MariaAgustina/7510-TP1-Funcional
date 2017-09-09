dd9e115449b8e0c8a5856a12f49476ce0f587838
Maria Agustina Markosich <ma.markosich@yahoo.com.ar>
2017-09-09 16:19:10 -0300

    casi terminados los rules, faltan ajustes

--- src/logical_interpreter.clj
+++ src/logical_interpreter.clj
@@ -1,26 +1,126 @@
 (ns logical-interpreter)
 
+(defn data-array-to-hash 
+    [dataArray]
+
+    (def keysVector(vector))
+   (doseq [query dataArray]
+      (def querykey (get (clojure.string/split query #"\((.*)") 0))
+;;      (println(clojure.string/split  querykey #"( *)(.*)"))
+      (def keysVector (conj keysVector querykey))
+      )
+
+   (def filteredKeysVector (distinct keysVector))
+
+   (def parsedMap (hash-map))
+   (doseq [filteredKey filteredKeysVector]
+      (def parsedMap (assoc parsedMap filteredKey (vector)))
+    )
+
+  (doseq [query dataArray]
+    (def querykey (get (clojure.string/split query #"\((.*)") 0))
+    (def vectorValue (get parsedMap querykey))
+    (def newVector (conj vectorValue query))  
+    (def parsedMap (assoc parsedMap querykey newVector))    
+  )
+  ;;(println parsedMap)
+  parsedMap
+)  
+
+
+(defn vector-has-string
+      [query,factsVector]
+    (if (some #(= query %) factsVector) 
+    "true"
+    "false"
+    )            
+)
+
+
+(defn get-result-for-fact
+        [query,factsVector]
+
+    (if (.endsWith query ")")
+      (vector-has-string (clojure.string/replace query ")" ").") factsVector)
+      (vector-has-string query factsVector)
+    )
+
+)
+
+(defn clear-vector
+      [vector]
+  (def lastParameter(get (clojure.string/split (last vector) #"\)(.*)")0))
+  (def vectorWithoutLastELement (pop vector))
+  (def clearedVector (conj vectorWithoutLastELement lastParameter))
+  clearedVector
+)
+
+(defn get-result-for-rule
+    [query,ruleVector,parsedMap]
+
+    (def rule (get ruleVector 0))
+    (def ruleFunction (get (clojure.string/split rule #":-")0))
+
+    (def parameters (get (clojure.string/split ruleFunction #"(.*)\(")1))
+    (def queryParameters (get (clojure.string/split query #"(.*)\(")1))
+
+    (def vectorQueryParamenters (clear-vector (clojure.string/split queryParameters #", ")))
+    (def vectorParamenters (clear-vector (clojure.string/split parameters #", ")))
+    
+    (def ruleWithTestParameters (clojure.string/replace rule "" ""))
+    (doseq [parameter vectorParamenters]
+     (def position (.indexOf vectorParamenters parameter))
+      (def queryParameter (get vectorQueryParamenters position))
+      (def ruleWithTestParameters (clojure.string/replace ruleWithTestParameters parameter queryParameter))
+    )
+
+    (def ruleWithTestParameters (clojure.string/replace ruleWithTestParameters "), " ") ,  "))
+    (def ruleFactsComponents (clojure.string/split (get (clojure.string/split ruleWithTestParameters #":-")1) #" , "))
+
+    (def result (vector))
+    (doseq [factComponentQuery ruleFactsComponents]
+
+      (def parsedFactsOrRuleVector (get parsedMap (str " " (get (clojure.string/split factComponentQuery #"\((.*)") 0) ) ) )
+       (if (get-result-for-fact (str " " factComponentQuery) parsedFactsOrRuleVector)
+          (def result (conj result "true"))  
+          (def result (conj result "false"))  
+        )
+    )
+
+    (println result)
+    (if (some #(= "false" %) result) 
+      (println "false")
+      (println "true")
+    )
+
+)
+
+(defn query-is-a-rule
+      [query,parsedFactsOrRuleVector]
+
+
+      (def firstElement (get parsedFactsOrRuleVector 0))
+      (if firstElement
+        (.contains firstElement ":-")
+        false
+      )
+)
+
 (defn evaluate-query
   
   "Returns true if the rules and facts in database imply query, false if not. If
   either input can't be parsed, returns nil"
   [database query]
 
-  (def factsMap (hash-map "varon" (vector "varon(juan)." "varon(pepe)." "varon(hector).") ,
-   							"mujer" (vector "mujer(maria)." "mujer(cecilia).") ,
-   							"a" "b"))
+  (def databaseVector (clojure.string/split-lines database))
+  (def parsedmap (data-array-to-hash databaseVector))
+  (def parsedFactsOrRuleVector (get parsedmap (str "  " (get (clojure.string/split query #"\((.*)") 0) )) )
+  ;;(println parsedFactsOrRuleVector)
 
-;;   (println factsMap)
-   
-   (def factsForQueryRule (get factsMap (get (clojure.string/split query #"\((.*)") 0) ) )
-	
-;;	(println factsForRule)
 
-	;;some devuelve nil si no encuentra el elemento en el vector, y devuelve true si lo encuentra
-	;;(println(some #(= query %) factsForQueryRule))
-	(if (some #(= query %) factsForQueryRule) 
-		"true"
-		"false"
-	)
+  (if (query-is-a-rule query parsedFactsOrRuleVector)
+    (get-result-for-rule query parsedFactsOrRuleVector parsedmap)
+    (get-result-for-fact (str "  " query) parsedFactsOrRuleVector)
+  )
 
   )
