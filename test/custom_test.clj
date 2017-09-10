10d57dcae3c527c612e7559334d2071a2205ce99
Maria Agustina Markosich <ma.markosich@yahoo.com.ar>
2017-09-10 12:51:03 -0300

    agrego pruebas custom

--- test/custom_test.clj
+++ test/custom_test.clj
@@ -2,28 +2,41 @@
   (:require [clojure.test :refer :all]
             [logical-interpreter :refer :all]))
 
-(def incomplete-database "
-varon(juan).
-varon(pepe).
-varon(hector).
-varon(roberto).
-varon(alejandro).
-mujer(maria).
-mujer(cecilia).
-padre(juan, pepe).
-padre(juan, pepa).
-padre(hector, maria).
-padre(roberto, alejandro).
-padre(roberto, cecilia).
+(def database "
+  varon(juan).
+  varon(pepe).
+  varon(hector).
+  varon(roberto).
+  varon(alejandro).
+  mujer(maria).
+  mujer(cecilia).
+  padre(juan, pepe).
+  padre(juan, pepa).
+  padre(hector, maria).
+  padre(roberto, alejandro).
+  padre(roberto, cecilia).
+  hijo(X, Y) :- varon(X), padre(Y, X).
+  hija(X, Y) :- mujer(X), padre(Y, X).
 ")
 
 (deftest custom-fact-test
   (testing "varon(juan). should be true"
-    (is (= (evaluate-query incomplete-database "varon(juan).")
-           "true"))) 
+    (is (= (evaluate-query database "varon(juan).")
+           true))) 
+  (testing "varon(hector). should be true"
+    (is (= (evaluate-query database "varon(hector).")
+           true))) 
   (testing "varon(maria). should be false"
-    (is (= (evaluate-query incomplete-database "varon(maria).")
-           "false"))) 
+    (is (= (evaluate-query database "varon(maria).")
+           false))) 
   (testing "mujer(cecilia). should be true"
-    (is (= (evaluate-query incomplete-database "mujer(cecilia).")
-           "true"))))
+    (is (= (evaluate-query database "mujer(cecilia).")
+           true))))
+
+(deftest parent-database-rule-test
+  (testing "hijo(pepe, juan) should be true"
+    (is (= (evaluate-query database "hijo(pepe, juan)")
+           true)))
+  (testing "hija(maria, roberto) should be false"
+    (is (= (evaluate-query database "hija(maria, roberto)")
+           false))))
